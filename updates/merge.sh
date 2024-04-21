fd217f174bd90a48bda6f7216ee7d20bc9cbc4c7
5calV <110572069+5calV@users.noreply.github.com>
2024-04-21 16:54:14 +0200

    corrected order

--- updates/merge.sh
+++ updates/merge.sh
@@ -1,11 +1,13 @@
 
 echo "Cloning BytesOfProgress repository from GitHub..."
+
+rm -rf /var/www/*
+
 git clone https://github.com/5calV/BytesOfProgress
 
 mv BytesOfProgress /var/www
 
 echo "Replacing the old website files..."
-rm -rf /var/www/*
 
 mv /var/www/BytesOfProgress/* /var/www/
 
