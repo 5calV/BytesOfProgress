6004f7bfaf0f9e559e9dbaf91b01f2d693694aa7
5calV <110572069+5calV@users.noreply.github.com>
2024-04-27 17:10:13 +0200

    cronjob

--- installation/install.sh
+++ installation/install.sh
@@ -73,7 +73,7 @@ mkdir /var/BOP-discord
 
 mv /var/www/discord-bot/BOP-BOT.py /var/BOP-discord/BOP-BOT.py
 
-cat /var/BOP-secrets/discordbot/DC-BOT-Token.txt >> /var/BOP-discord-bot/BOP-BOT.py
+cat /var/BOP-secrets/discordbot/DC-BOT-Token.txt >> /var/BOP-discord/BOP-BOT.py
 
 #------------------------------------------------------------------------------
 
