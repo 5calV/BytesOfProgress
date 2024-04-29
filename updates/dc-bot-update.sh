c49c239a5e1fca72df955f3364be504a2d478838
5calV <110572069+5calV@users.noreply.github.com>
2024-04-29 20:34:12 +0200

    added wait time before going back to system util

--- updates/dc-bot-update.sh
+++ updates/dc-bot-update.sh
@@ -13,4 +13,8 @@ cp /var/www/discord-bot/BOP-BOT.py /var/BOP-discord/BOP-BOT.py
 
 cat /var/BOP-secrets/discordbot/DC-BOT-Token.txt >> /var/BOP-discord/BOP-BOT.py
 
+echo
+
 echo 'Please restart the bot with BOP System Utility.'
+
+sleep 3
