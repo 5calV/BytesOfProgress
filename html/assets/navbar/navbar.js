37fc22e7b22ae9cf567649720b867600a66a1173
5calV <110572069+5calV@users.noreply.github.com>
2024-06-17 15:44:15 +0200

    fixed navbar scrolling issue

--- html/assets/navbar/navbar.js
+++ html/assets/navbar/navbar.js
@@ -5,28 +5,32 @@
 //  \(͡⚈ᴗ͡⚈)/
 
 //-------------------------------------------------------------------------------------------------------------------------------------*/
-
 document.addEventListener("DOMContentLoaded", function() {
     const toggleButton = document.getElementById("navbarOpenButton");
     const navbarContainer = document.getElementById("navbarContainer");
 
     // Überprüfen, ob toggleButton und navbarContainer erfolgreich gefunden wurden
     if (toggleButton && navbarContainer) {
-      console.log("Found toggleButton and navbarContainer")
         toggleButton.addEventListener("click", function() {
             if (navbarContainer.classList.contains("navbar-open")) {
-                // close navbar
+                // Navbar schließen
                 navbarContainer.classList.remove("navbar-open");
                 navbarContainer.style.width = "0";
-                console.log("Navbar closed");
+                console.log("Navbar geschlossen");
             } else {
-                // open navbar
+                // Navbar öffnen
                 navbarContainer.classList.add("navbar-open");
                 navbarContainer.style.width = "475px";
-                console.log("Navbar opened");
+                console.log("Navbar geöffnet");
             }
         });
+
+        // Scrollverhalten der Hauptseite überwachen
+        window.addEventListener("scroll", function() {
+            const scrollPosition = window.scrollY;
+            navbarContainer.style.top = scrollPosition + "px";
+        });
     } else {
-        console.error("toggleButton or navbarContainer not found");
+        console.error("toggleButton oder navbarContainer nicht gefunden");
     }
 });
