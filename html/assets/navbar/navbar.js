52c369c2d8a99ae95b4652a411de5118ef7109dd
5calV <110572069+5calV@users.noreply.github.com>
2024-06-17 09:34:35 +0200

    added debugging message

--- html/assets/navbar/navbar.js
+++ html/assets/navbar/navbar.js
@@ -15,10 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
             // close navbar
             navbarContainer.classList.remove("navbar-open");
             navbarContainer.style.width = "0";
+            console.log("Navbar closed");
         } else {
             // open navbar
             navbarContainer.classList.add("navbar-open");
             navbarContainer.style.width = "475px";
+            console.log("Navbar opened");
         }
     });
 });
