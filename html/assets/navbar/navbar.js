080a26b9c14a59562c0fa551996f57dfd967e100
5calV <110572069+5calV@users.noreply.github.com>
2024-06-17 09:54:14 +0200

    debugging

--- html/assets/navbar/navbar.js
+++ html/assets/navbar/navbar.js
@@ -10,17 +10,23 @@ document.addEventListener("DOMContentLoaded", function() {
     const toggleButton = document.getElementById("navbarOpenButton");
     const navbarContainer = document.getElementById("navbarContainer");
 
-    toggleButton.addEventListener("click", function() {
-        if (navbarContainer.classList.contains("navbar-open")) {
-            // close navbar
-            navbarContainer.classList.remove("navbar-open");
-            navbarContainer.style.width = "0";
-            console.log("Navbar closed");
-        } else {
-            // open navbar
-            navbarContainer.classList.add("navbar-open");
-            navbarContainer.style.width = "475px";
-            console.log("Navbar opened");
-        }
-    });
+    // Überprüfen, ob toggleButton und navbarContainer erfolgreich gefunden wurden
+    if (toggleButton && navbarContainer) {
+      console.log("Found toggleButton and navbarContainer")
+        toggleButton.addEventListener("click", function() {
+            if (navbarContainer.classList.contains("navbar-open")) {
+                // close navbar
+                navbarContainer.classList.remove("navbar-open");
+                navbarContainer.style.width = "0";
+                console.log("Navbar closed");
+            } else {
+                // open navbar
+                navbarContainer.classList.add("navbar-open");
+                navbarContainer.style.width = "475px";
+                console.log("Navbar opened");
+            }
+        });
+    } else {
+        console.error("toggleButton or navbarContainer not found");
+    }
 });
