ec80f07510f2715f92dbdff448270252f1e2a9cb
5calV <110572069+5calV@users.noreply.github.com>
2024-07-29 07:46:15 +0200

    included cgi script in update

--- updates/merge.sh
+++ updates/merge.sh
@@ -7,6 +7,10 @@ git clone https://github.com/5calV/BytesOfProgress
 
 mv BytesOfProgress /var/www
 
+rm /usr/lib/cgi-bin/monitoring.sh
+
+mv /var/www/installation/management-panel/monitoring.sh /usr/lib/cgi-bin/monitoring.sh
+
 echo "Replacing the old website files..."
 
 mv /var/www/BytesOfProgress/* /var/www/
