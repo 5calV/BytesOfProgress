d823b2d9bb075e6fe65c0fff4859f1f4d4015f6e
5calV <berwingcalvin@gmail.com>
2024-08-20 14:58:18 +0200

    testing

--- updates/merge.sh
+++ updates/merge.sh
@@ -5,16 +5,16 @@ rm -rf /var/www/*
 
 git clone https://github.com/5calV/BytesOfProgress
 
-mv BytesOfProgress/* /var/www/
+mkdir /var/temp
+
+mv BytesOfProgress /var/temp
+
+mv /var/temp/BytesOfProgress/* /var/www/
 
 echo "Replacing the old website files..."
 
-mkdir /var/www/management
-
-cp /var/www/maintenance/management-panel/* /var/www/management/
-
 echo "Removing repository"
-rm -rf /var/www/BytesOfProgress
+rm -rf /var/temp
 
 chmod +x /var/www/maintenance/BOP-system-util.sh
 
